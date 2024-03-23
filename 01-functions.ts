0d39791f03fc5055504070d994e05f076b81eb9d
Prayag <prayagbhatt2003@gmail.com>
2024-03-23 11:18:03 +0530

    added new functions

--- 01-functions.ts
+++ 01-functions.ts
@@ -10,6 +10,16 @@ function userSignUp(name: string, email: string, password: string) {
   return { name, email, password };
 }
 
+let userLogin = (
+  email: string,
+  password: string,
+  isLogged: boolean = false
+) => {
+  return { email, password, isLogged };
+};
+
 addTwo(5);
 getUpper("hello");
 userSignUp("Prayag", "anandvishy387@gmail.com", "123");
+
+export {};
