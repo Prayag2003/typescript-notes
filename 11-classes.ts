038e38648c4a23cefde1b083278214b25dab2308
Prayag <prayagbhatt2003@gmail.com>
2024-03-25 19:51:46 +0530

    access modifiers added

--- 11-classes.ts
+++ 11-classes.ts
@@ -10,12 +10,36 @@
 
 // NOTE: Much more cleaner way to write in Ts
 class User {
+  private _courseCount = 1;
   constructor(
     public name: string,
-    public email: string,
-    private userId: string
-  ) {}
+    public email: string // private userId: string
+  ) {
+    this.name = name;
+    this.email = email;
+  }
+
+  private deleteToken(): void {
+    console.log("Token deleted");
+  }
+
+  // NOTE: Getter
+  get googleEmailId(): string {
+    return `${this.email}@google.com`;
+  }
+
+  get courseCount(): number {
+    return this._courseCount;
+  }
+
+  // NOTE: Setter
+  set courseCount(courseNum) {
+    if (courseNum <= 1) {
+      throw new Error("Course count should be greater than 1");
+    }
+    this._courseCount = courseNum;
+  }
 }
 
-const user = new User("pb123@gmail.com", "prayag", "123");
+const user = new User("pb123@gmail.com", "prayag");
 export {};
